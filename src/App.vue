c5e396e88bdde0efb437d8d9386b0d05c67d9c67
lotarevivan <lotarevivan@gmail.com>
2022-05-05 12:27:02 +0300

    разметка банковской карты

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,79 @@
 <template>
-<div>
-  <ul class="mdc-image-list my-image-list">
-  <li class="mdc-image-list__item">
-    <div class="mdc-image-list__image-aspect-container">
-      <img class="mdc-image-list__image" >
+  <section class="cards__container">
+    <div class="cards__item">
+      <div class="cards__body">
+        <div class="cards__body-logo">logo</div>
+        <div class="cards__body-number">
+          <p>1234</p>
+          <p>1234</p>
+          <p>1234</p>
+          <p>1234</p>
+        </div>
+        <div class="cards__body-valid">
+          <div>valid/true</div>
+          <div>00/00</div>
+        </div>
+        <div class="cards__body-name">
+          <div>adasdad</div>
+        </div>
+      </div>
     </div>
-    <div class="mdc-image-list__supporting">
-      <span class="mdc-image-list__label">Text label</span>
-    </div>
-  </li>
-  <li class="mdc-image-list__item">
-    <div class="mdc-image-list__image-aspect-container">
-      <img class="mdc-image-list__image" >
-    </div>
-    <div class="mdc-image-list__supporting">
-      <span class="mdc-image-list__label">Text label</span>
-    </div>
-  </li>
-  <li class="mdc-image-list__item">
-    <div class="mdc-image-list__image-aspect-container">
-      <img class="mdc-image-list__image" >
-    </div>
-    <div class="mdc-image-list__supporting">
-      <span class="mdc-image-list__label">Text label</span>
-    </div>
-  </li>
-  
-</ul>
-</div>
+  </section>
 </template>
 
 <script>
-
 export default {
   name: 'App',
 }
 </script>
 
-<style lang='scss'>
-@use "@material/image-list/mdc-image-list";
+<style lang="scss">
+.cards {
+  &__container {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 90mm);
+    // grid-auto-flow:column;
+    grid-gap: 20px;
+  }
+  &__item {
+    padding: 5px;
+  }
+}
+.cards__body {
+  height: 55mm;
+  width: 86mm;
+  border: 1px solid black;
+  border-radius: 12px;
+  display: grid;
+  position: relative;
+  padding: 20px;
+  box-sizing: border-box;
+  &-number {
+    border: 1px solid green;
+    align-self: center;
+    display: flex;
+    justify-content: space-between;
+    line-height: 1;
+  }
+  &-valid {
+    border: 1px solid rgb(176, 19, 215);
+    align-self: center;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
 
+    & > *:not(:last-child) {
+      margin-right: 2em;
+    }
+  }
+  &-name {
+    align-self: center;
+    border: 1px solid rgb(10, 13, 112);
+  }
+  &-logo {
+    align-self: center;
+    text-align: center;
+    border: 1px solid rgb(10, 226, 68);
+  }
+}
 </style>
